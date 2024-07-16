e7e5515028a6535e404e51fdd35eae13a943133e
Artem Rudov <dovakin2281337@gmail.com>
2024-07-16 15:42:27 +0300

    added env and controllers

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { AppDataSource } from "../db/data-source";
+import { addTask, deleteTask } from "../controllers/todosController";
 
 const router = express.Router()
+const todosRepo = AppDataSource.getRepository('Todos')
 
 router.get('/', (req, res) => {
   res.send('Я тут')
 })
 
+router.post('/create', addTask)
+
+router.post('/:id/delete', deleteTask)
 export default router
