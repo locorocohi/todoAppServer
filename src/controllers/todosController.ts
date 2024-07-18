90247b49057b21017f6fa679cb9bb18b1ee83e4c
Artem Rudov <dovakin2281337@gmail.com>
2024-07-18 12:16:50 +0300

    added asyncHandler and types for controllers

--- src/controllers/todosController.ts
+++ src/controllers/todosController.ts
@@ -1,81 +1,47 @@
 import { Todo } from "../db/entity/Todo";
 import { todosRepo } from "../db";
+import type { RequestHandler } from 'express';
+import asyncHandler from "express-async-handler";
 
-export const addTask = async (req, res, next) => {
-  try {
-    const newTodo = new Todo();
-    newTodo.text = req.body.text;
-    newTodo.completed = req.body.completed;
-    await todosRepo.save(newTodo)
-    res.json('Done!')
+export const addTask: RequestHandler = asyncHandler(async (req, res, next) => {
+  const newTodo = new Todo();
+  newTodo.text = req.body.text;
+  newTodo.completed = req.body.completed;
+  await todosRepo.save(newTodo)
+  res.json('Done!')
+})
 
-  } catch(err) {
-    console.log(err)
-    next()
+export const getArrayWithTodos: RequestHandler = asyncHandler(async (req, res, next) => {
+  const todosArr = await todosRepo.find()
+  res.json(todosArr)
+})
+
+export const findTaskById: RequestHandler = asyncHandler(async (req, res, next) => {
+  const taskId = req.params.id
+  const currTask = await todosRepo.findOneBy({id: taskId})
+  res.json(currTask)
+})
+
+export const editTask: RequestHandler = asyncHandler(async (req, res, next) => {
+  const taskId = req.params.id
+  const currTask = await todosRepo.findOneBy({id: taskId})
+  currTask.text = req.body.text
+  await todosRepo.save(currTask)
+  res.json(currTask)
+})
+
+export const deleteTask: RequestHandler = asyncHandler(async (req, res, next) => {
+  const taskId = req.params.id
+  await todosRepo.delete(taskId)
+  res.json('Task deleted')
+})
+
+export const deleteCompleted: RequestHandler = asyncHandler(async (req, res, next) => {
+  const completedTasks = await todosRepo.find({where: {completed: true},})
+  if(completedTasks.length) {
+    completedTasks.forEach(async item => {
+      await todosRepo.delete(item)
+    })
   }
-}
-
-export const getArrayWithTodos = async (req, res, next) => {
-  try {
-    const todosArr = await todosRepo.find()
-    res.json(todosArr)
-
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-}
-
-export const findTaskById = async (req, res, next) => {
-  try {
-    const taskId = req.params.id
-    const currTask = await todosRepo.findOneBy({id: taskId})
-    res.json(currTask)
-
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-}
-
-export const editTask = async (req, res, next) => {
-  try {
-    const taskId = req.params.id
-    const currTask = await todosRepo.findOneBy({id: taskId})
-    currTask.text = req.body.text
-    await todosRepo.save(currTask)
-    res.json(currTask)
-
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-}
-
-export const deleteTask = async (req, res, next) => {
-  try {
-    const taskId = req.params.id
-    await todosRepo.delete(taskId)
-    res.json('Task deleted')
-
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-}
-
-export const deleteCompleted = async (req, res, next) => {
-  try {
-    const completedTasks = await todosRepo.find({where: {completed: true},})
-    if(completedTasks.length) {
-      completedTasks.forEach(async item => {
-        await todosRepo.delete(item)
-      })
-    }
-    res.json('Completed were deleted')
-
-  } catch (err) {
-    console.log(err)
-    next()
-  }
-}
+  res.json('Completed were deleted')
+})
